bc5385f5b1c92f9755e66fcd054b6a3c5f03fd66
rohithdesikan <31898229+rohithdesikan@users.noreply.github.com>
2019-03-12 00:05:22 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ import requests
 import geopandas as gpd
 
 # Data Visualization Packages
-
-# Matplotlib and Seaborn
-
 # Bokeh
 import bokeh as bk
 from bokeh.io import output_notebook, show, output_file, save
@@ -27,26 +24,10 @@ from bokeh.models import (ColumnDataSource as cds, Plot, DatetimeAxis, PanTool, 
                           tickers, BoxAnnotation, Panel, Range1d, LabelSet, Label, NumeralTickFormatter,
                           LogColorMapper, GeoJSONDataSource, LinearColorMapper, ColorBar,
                           LogTicker, BasicTicker, CategoricalColorMapper, FixedTicker, AdaptiveTicker)
-from bokeh.palettes import viridis, magma, inferno, cividis, Greens, Blues, PuRd, YlOrRd, YlOrBr, RdYlGn, RdYlGn8 as palette
+from bokeh.palettes import RdYlGn6 as palette
 from bokeh.embed import file_html, components
 from bokeh.layouts import layout, gridplot
 from bokeh.resources import INLINE, CDN
-
-# output_notebook()
-
-# Import Machine Learning Packages
-from sklearn import base
-from sklearn import metrics
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV, ShuffleSplit
-from sklearn.pipeline import Pipeline
-from sklearn import pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import normalize
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import Ridge, LinearRegression
-from sklearn.svm import SVR
-from sklearn.pipeline import FeatureUnion
 
 # --------------------------------------------------------------
 
@@ -85,138 +66,71 @@ def style(p):
 # --------------------------------------------------------------
 ######### Function to pull data and clean based on user input
 def obtain_clean_data():
-    replica_final1 = pd.read_csv('replica_final1.csv')
-    replica_final2 = pd.read_csv('replica_final2.csv')
-#     replica_final3 = pd.read_csv('replica_final3.csv')
-#     replica_final4 = pd.read_csv('replica_final4.csv')
-#     replica_final5 = pd.read_csv('replica_final5.csv')
-#     replica_final = pd.concat([replica_final1, replica_final2, replica_final3, replica_final4, replica_final5], axis = 0, ignore_index = False)
-    replica_final = pd.concat(['replica_final1,replica_final2], axis = 0, ignore_index = False)
-    replica_final.drop('Unnamed: 0', axis = 1, inplace = True)
-    ypred_train_gbr_lidar = dill.load(open('ypred_train_gbr_lidar.pkd', 'rb'))
-    ypred_test_gbr_lidar = dill.load(open('ypred_test_gbr_lidar.pkd', 'rb'))
-    ypred_train_dem = dill.load(open('ypred_train_dem.pkd', 'rb'))
-    ypred_test_dem = dill.load(open('ypred_test_dem.pkd', 'rb'))
-
-
-    replica_f = replica_final.fillna(method='ffill', axis = 0)
-
-    # Find the columns within the data and send them to a list
-    replica_cols = replica_f.columns.tolist()
-
-    # Split at company_ty which is the split between lidar and demographic
-    cty = replica_cols.index('company_ty')
-
-    r_lidar = replica_f.iloc[:,:cty]
-    r_lidar.drop(['state_abbr', 'county_name', 'centroid_x', 'centroid_y', 'polygons'], axis = 1, inplace = True)
-    r_demographic = replica_f.iloc[:,cty:-2]
-
-    savings = replica_f['Savings [$/yr]']
-
-    # Set up Lidar data ML sets
-    X_train_lidar, X_test_lidar, y_train_lidar, y_test_lidar = train_test_split(r_lidar,
-                                                                                savings,
-                                                                                test_size = 0.2,
-                                                                                random_state = 42)
-    y_train_lidar_indices = y_train_lidar.index.values.tolist()
-    y_test_lidar_indices = y_test_lidar.index.values.tolist()
-
-        # Set up demographic ML sets
-    X_train_dem, X_test_dem,  y_train_dem, y_test_dem = train_test_split(r_demographic,
-                                                                        savings,
-                                                                        test_size = 0.2,
-                                                                        random_state = 42)
-
-
-    train_one_hot = pd.get_dummies(X_train_dem[['company_ty',
-                                            'climate_zone_description',
-                                            'moisture_regime',
-                                            'locale']])
-    X_train_dem = X_train_dem.join(train_one_hot)
-    X_train_dem.drop(['company_ty', 'climate_zone_description', 'moisture_regime', 'locale'], axis = 1, inplace = True)
-
-    test_one_hot = pd.get_dummies(X_test_dem[['company_ty',
-                                            'climate_zone_description',
-                                            'moisture_regime',
-                                            'locale']])
-    X_test_dem = X_test_dem.join(test_one_hot)
-    X_test_dem.drop(['company_ty', 'climate_zone_description', 'moisture_regime', 'locale'], axis = 1, inplace = True)
-
-    y_train_dem_indices = y_train_lidar.index.values.tolist()
-    y_test_dem_indices = y_test_lidar.index.values.tolist()
-
-
-    X_train_ensemble = np.vstack((ypred_train_gbr_lidar, ypred_train_dem)).T
-    y_train_ensemble = y_train_lidar.values.tolist()
-    X_test_ensemble = np.vstack((ypred_test_gbr_lidar, ypred_test_dem)).T
-    y_test_ensemble = y_test_lidar.values.tolist()
-
-    # Build the ensemble model
-    ensemble_linear = Ridge(alpha = 2, fit_intercept = True)
-    ensemble_linear.fit(X_train_ensemble, y_train_ensemble)
-    ypred_train_ensemble = ensemble_linear.predict(X_train_ensemble)
-    ypred_test_ensemble = ensemble_linear.predict(X_test_ensemble)
-
-
-    replica_small = replica_final[['geoid', 'state_abbr', 'county_name' , 'polygons', 'avg_yearly_bill_dlrs']]
-    replica_train = replica_small.iloc[y_train_lidar_indices, :]
-    replica_train['Savings_Predictions'] = ypred_train_ensemble
-
-    replica_test = replica_small.iloc[y_test_lidar_indices, :]
-    replica_test['Savings_Predictions'] = ypred_test_ensemble
-    replica_plot = pd.concat([replica_train, replica_test], axis = 0)
-
-    # Turning replica data frame into a geopandas dataframe
-    replica_plot.rename({'polygons':'geometry'},inplace=True,axis=1)
-    replica_geoplot = gpd.GeoDataFrame(replica_plot, geometry='geometry')
-
-    # # Chose the demographic subset of the data
-    # replica_cols = replica_final.columns.tolist()
-    #
-    # plys = replica_cols.index('polygons')
-    # cty = replica_cols.index('company_ty')
-    # locale = replica_cols.index('locale')
-    #
-    # r_base = replica_final.iloc[:,:plys]
-    # r_dems = replica_final.iloc[:,cty:locale+1]
-    #
-    # rdems = pd.concat([r_base,r_dems],axis=1)
-    # rdems[['Savings [$/yr]','Savings [% Annual Income]']] = replica_final.iloc[:,-2:]
-    #
-    # # Work with the Demographic Subset to get Mean HH Income and Total Population
-    # rdems2 = replica_final[['geoid','state_abbr','county_name','polygons','hh_med_income','pop_total']]
-    #
-    #
-    # # Choose the lidar subset of the data
-    # r_lid = replica_final.iloc[:,:cty]
-    # r_end = replica_final.iloc[:,locale+1:]
-    # rlidar = pd.concat([r_lid,r_end],axis=1)
-    #
-    # # Work with the Lidar Subset to get dev roof area and MWH gen potential
-    # rlidar2 = rlidar.copy()
-    # # Choose the Multifamily vs Single Family subset of the data
-    # devp_m2_cols = [x for x in rlidar2.columns[rlidar2.columns.str.contains('devp_m2')]]
-    # hh_cols = [x for x in rlidar2.columns[rlidar2.columns.str.contains('hh')]]
-    # mwh_cols = [x for x in rlidar2.columns[rlidar2.columns.str.contains('mwh')]]
-    #
-    # rlidar2['hh'] = rlidar2[hh_cols].sum(axis=1)
-    # rlidar2['devp_m2'] = rlidar2[devp_m2_cols].sum(axis=1)
-    # rlidar2['mwh'] = rlidar2[mwh_cols].sum(axis=1)
-    # rlidar2 = rlidar2[['hh','devp_m2','mwh','Savings [% Annual Income]']]
-    #
-    #
-    # rplot = pd.concat([rdems2,rlidar2],axis=1)
-    #
-    #
     # USA Basemap File
-    file = '/Users/rohithdesikan/Desktop/Data Analysis/The Data Incubator/Capstone Project/states_21basic/states.shp'
+    replica_fewplot = dill.load(open('replica_few_plot.pkd', 'rb'))
+    file = 'states_21basic/states.shp'
     mapdf = gpd.read_file(file)
-    #
-    # # Turning replica data frame into a geopandas dataframe
-    # rplot.rename({'polygons':'geometry'},inplace=True,axis=1)
-    # rgeo = gpd.GeoDataFrame(rplot, geometry='geometry')
 
-    return mapdf, replica_geoplot
+    return mapdf, replica_fewplot
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+######### Function to graph exploratory data
+def create_exploratory_graph(replica_few_plot):
+    # mapdf: Basemap of the US
+    # rgeo: Replica data as a geo data frame
+    # statef: The state chosen by the user
+
+    replica_few_plot1 = pd.DataFrame(replica_few_plot)
+    replica_few_plot2 = replica_few_plot1.drop('geometry', axis = 1)
+    colormap = {'NV': 'green', 'UT': 'red'}
+    colors = [colormap[x] for x in replica_few_plot2['state_abbr']]
+    replica_few_plot2['Colors']=colors
+
+    src = cds(replica_few_plot2)
+
+    p1 = figure(plot_width=1000, plot_height=600,
+       x_axis_label = 'Total Annual Solar Potential [GWh]',
+       y_axis_label ='Observed Annual Savings [$/year]',
+       title = 'Relationship between Annual Solar Savings and Solar Irradiation',
+       tools="save,pan,undo,wheel_zoom,box_zoom,reset")
+
+    p1.scatter(x='gwh',
+               y='Savings [$/yr]',
+               size=6, fill_color='Colors',
+               line_color = 'black',
+               alpha=0.8,
+               source=src,
+               legend='state_abbr')
+
+
+    # Add hover tool functionality
+    p1.add_tools(HoverTool(
+        tooltips=[
+            ('State',                        '@state_abbr'),
+            ('Total Annual Solar Potential [GWh]',      '@gwh'),
+            ( 'Observed Savings',         '@{Savings [$/yr]}{1.11}'), # use @{ } for field names with spaces
+        ],
+        formatters={
+            'gwh'     : 'numeral', # use 'datetime' formatter for 'date' field
+            '{Savings [$/yr]}'  : 'numeral',   # use 'printf' formatter for 'OADB ' field
+        },
+        mode='mouse'
+    ))
+
+    # Add labels
+    p1.toolbar_location = 'right'
+    p1.legend.location = "top_right"
+
+    style(p1)
+
+    grid = gridplot([p1], ncols = 2)
+
+    # show(grid)
+
+    script, div = components(grid)
+
+    return grid, script, div
 # --------------------------------------------------------------
 
 
@@ -262,23 +176,17 @@ def create_graph(mapdf,rgeo,statef):
     # Make the geoJSON datasource
     geo_source = GeoJSONDataSource(geojson=rjson)
 
-
-    # Plot on Bokeh outside of a Function
-    # n=6 # Number of colors on the choropleth and legend
-
-    # Plot the Predictions from the ML Model once they are done.
-    # Plot on Bokeh outside of a Function
-
-    # Set the colormapper
-    # Greens1 = Greens[n]
-    # Greens2 = Greens1[::-1]
-
     palette.reverse()
     cmapper = LinearColorMapper(palette=palette,
                                 low=rstate_df['Savings_Predictions'].min(),
                                 high=rstate_df['Savings_Predictions'].max())
 
-    color_bar = bk.models.ColorBar(color_mapper=cmapper, ticker=BasicTicker(desired_num_ticks=8),
+    low = rstate_df['Savings_Predictions'].min()
+    high = rstate_df['Savings_Predictions'].max()
+    diff = high - low
+    ticks = [int(diff/6), int(diff/3), int(diff/2), int(2*diff/3), int(5*diff/6), int(diff)]
+
+    color_bar = bk.models.ColorBar(color_mapper=cmapper, ticker=BasicTicker(desired_num_ticks = 6),
                      label_standoff=12, border_line_color=None, location=(0,0))
 
     # Set up the figure
@@ -325,8 +233,6 @@ def create_graph(mapdf,rgeo,statef):
     # p.legend.click_policy="hide"
 
     style(ppreds)
-    # show(ppreds)
-
 
 
     # grid = gridplot([p1, p2, p3, p4, ppreds], ncols=2, plot_width=fig_width, plot_height=fig_height)
@@ -362,6 +268,12 @@ def about():
 def flowchart():
   return render_template('flowchart.html')
 
+@app.route('/explore')
+def explore():
+    mapdf, replica_geoplot = obtain_clean_data()
+    grid,script,div = create_exploratory_graph(replica_geoplot)
+    # return render_template('stock.html', graph_num=plot)
+    return Markup(file_html(grid,resources=CDN,template='explore.html'))
 
 ######### State Page
 @app.route('/state',methods =['GET','POST'])
